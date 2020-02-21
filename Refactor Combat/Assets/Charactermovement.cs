48ec88e157ac185af0ba67ab16aa16b616c643e6
Marco <marco.graffi15@gmail.com>
2020-02-21 15:08:17 -0300

    Prototype

---
+++ Refactor Combat/Assets/Charactermovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charactermovement : MonoBehaviour
+{
+    private float movementSpeed = 5f;
+    public void Start()
+    {
+        
+    }
+    void Update()
+    {
+    float horizontalInput = Input.GetAxis("Horizontal");
+    float verticalInput = Input.GetAxis("Vertical");
+
+    transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0, verticalInput * movementSpeed * Time.deltaTime);
+
+    Debug.Log (transform.position);
+    }
+}
---
+++ Refactor Combat/Assets/TimelineController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+    void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            timeline.Stop();
+        }
+        
+    }
+    void OnTriggerEnter(Collider c)
+    {   if (c.gameObject.tag == "Player")
+    {
+        timeline.Play();
+        
+    }
+        
+    }
+
+    
+}
